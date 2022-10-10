ddb91c91abbecbb9fa2e7b3f902cd20d0ec91470
ananiabahru <ananiabaharu13@gmail.com>
2022-10-10 12:23:59 -0700

    add all

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,12 +15,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
             div = 0
-        except ZeroDivisionError:
-            print("division by 0")
-            div = 0
-        except ZeroDivisionError:
-            print("division by 0")
-            div = 0
         except IndexError:
             print("out of range")
             div = 0
