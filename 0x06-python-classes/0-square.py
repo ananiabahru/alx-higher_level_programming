579a1ee36aa08a082c78ac8f3b0b0df14e7ee115
ananiabahru <ananiabaharu13@gmail.com>
2022-10-11 13:29:17 -0700

    task 0- 3

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-square.py by Anania Baharu
+"""Defines a square """
+
+class Square:
+    """nothing"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,5 @@
+#This is a readme file for my project 0x06-python-classes
+
+task 0- task 10
+
+#end of task
