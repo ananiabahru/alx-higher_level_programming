ffb4932569522f6e5dcf22a2da178a5500addbc6
ananiabahru <ananiabaharu13@gmail.com>
2022-10-11 15:56:31 -0700

    add all

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
-# 3-square.py by Anania baharu
-""" defines a 3square """
+# 3-square.py By Anania Baharu
+"""Define a class Square."""
 
-class square:
-    """A class that represents a square"""
+class Square:
 
-def __int__(self, size=0):
-    """Initializing this 3square class
-    Args:
-        size (int): The size of the new square.
-    """
-    
-    if not isinstance(size, int):
-        raise TypeError('size must be an integer')
-    elif size < 0:
-        raise ValueError('size must be >= 0')
-    self.__size = size
+    """Represent a square."""
 
-def area(self):
-    """
-    Calculate area of the square
-    Returns: The square of the size
-    """
 
-    return (self.__size ** 2)
+
+    def __init__(self, size=0):
+
+        """Initialize a new square.
+
+        Args:
+
+            size (int): The size of the new square.
+
+        """
+
+        if not isinstance(size, int):
+
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+
+
+    def area(self):
+
+        """Return the current area of the square."""
+
+        return (self.__size * self.__size)
