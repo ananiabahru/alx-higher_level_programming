f134717d91daa21204b81ae1ce703779c8c9f017
ananiabahru <ananiabaharu13@gmail.com>
2022-10-18 13:01:20 -0700

    add

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,8 @@
 """this defines a locked class py"""
 
 class LockedClass:
-"""
-Only allows instatiation of an attribute called first_name
-"""
-__slots__ = ["first_name"]
+    """
+    Only allows instatiation of an attribute called first_name
+    """
+
+    __slots__ = ["first_name"]
