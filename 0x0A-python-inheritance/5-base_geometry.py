b14531501b0c740bd19beaf7d0c9413edeb4a278
ananiabahru <ananiabaharu13@gmail.com>
2022-10-24 16:08:42 -0700

    add

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""empty class"""
+
+
+
+
+
+class BaseGeometry:
+
+    """empty"""
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""public instance method"""
+
+
+
+
+
+class BaseGeometry:
+
+    """public instance method"""
+
+    def area(self):
+
+        """raises an exception"""
+
+        raise Exception("area() is not implemented")
