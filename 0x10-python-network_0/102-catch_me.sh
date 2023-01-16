ab7027c998fd88daea0f863dd049ddca84223bf5
ananiabahru <ananiabaharu13@gmail.com>
2023-01-16 15:41:28 -0800

    add

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Follows a URL for it to respond with " you got me!"
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me:wq!
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
 
 
